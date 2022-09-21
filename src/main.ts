058c87109c6a90239ceea547c524003230362751
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2022-09-22 01:46:09 +0900

    feat: show empty dialog

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
-function onOpen() {}
+function onOpen() {
+  SpreadsheetApp.getUi() // Or DocumentApp or SlidesApp or FormApp.
+    .createMenu("Custom Menu")
+    .addItem("Show dialog", "showDialog")
+    .addToUi();
+}
+
+function showDialog() {
+  var html = HtmlService.createHtmlOutputFromFile("Dialog")
+    .setWidth(400)
+    .setHeight(300);
+  SpreadsheetApp.getUi() // Or DocumentApp or SlidesApp or FormApp.
+    .showModalDialog(html, "My custom dialog");
+}
